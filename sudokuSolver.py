3c08e9e471cd17bcff39882fd212cce987a0dd5d
simplydoesnot <emorris.mxi@gmail.com>
2021-05-31 07:19:27 -0700

    Completed the web scraping feature

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -137,10 +137,28 @@ def main():
     # the .pagesource tells the beautifulsoup to get the html contents off the opened web browser
     soup = BeautifulSoup(driver.page_source, 'lxml')
 
+    # find the element named table
+    table = soup.find('table', class_='t')
 
+    # nested for loop to web scrape the Sudoku puzzle on the web page
+    # looking for all elements within table labeled tr
+    for element in table.find_all('tr'):
+        # a 2nd list to hold the 9 values per row
+        ints = []
+        # looking for all html elements within tr labeled input
+        for number in element.find_all('input'):
+            # try/ Exception to catch if there is no html element labeled value
+            try:
+                # place the value into the ints list
+                ints.append(int(number['value']))
+            except Exception as e:
+                # If there is no element labeled value place a zero at that index
+                value = 0
+                # place a zero into the ints list
+                ints.append(value)
+        # append the completed row into the list grid
+        grid.append(ints)
 
-
-    
     print()
     printSudoku(grid, 'unsolved')
 
