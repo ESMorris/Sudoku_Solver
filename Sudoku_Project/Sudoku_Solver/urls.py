e84829d617d6ad9122216cb940a1106977521a02
simplydoesnot <emorris.mxi@gmail.com>
2021-10-22 01:21:50 -0700

    Added a start/ url for the Sudoku Solver

--- Sudoku_Project/Sudoku_Solver/urls.py
+++ Sudoku_Project/Sudoku_Solver/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='Sudoku'),
+    path ('Start/', views.start, name='Start'),
     path('Generate/', views.GenerateButton, name='Generate'),
     path('Solve/', views.SolveButton, name='Solve'),
 ]
