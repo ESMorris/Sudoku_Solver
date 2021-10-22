9402fd43d833a6a49e66a1c4177f63fecbcfbdce
simplydoesnot <emorris.mxi@gmail.com>
2021-10-22 01:23:33 -0700

    Finalized the code for the sudoku solver to run

--- Sudoku_Project/Sudoku_Solver/views.py
+++ Sudoku_Project/Sudoku_Solver/views.py
@@ -1,22 +1,39 @@
 from django.http import response, JsonResponse
 from django.shortcuts import render
 import json
+from Sudoku_Solver.Helpers import web_scraping as wb
+from Sudoku_Solver.Helpers import Solver as Slvr
 
 # Create your views here.
 # where our game lives in the home route
 def home(request):
     return render(request, 'Sudoku_Solver/index.html')
 
+def start(request):
+    result = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    jsonStr = json.dumps(result)
+    return JsonResponse(jsonStr, safe=False)
+
 # Button that will generate the unsolved Sudoku
 def GenerateButton(response):
-    responseData = [
-        [1, 2, 3, 4, 5],
-        [6, 7, 8, 9, 10],
-        [11, 12, 13, 14, 15]
-    ]
-    jsonStr = json.dumps(responseData)
+    grid = []
+    global result
+    result = wb.web_scraping(grid)
+    jsonStr = json.dumps(result)
     return JsonResponse(jsonStr, safe=False)
 
 # Button that will solve the unsolved Sudoku
 def SolveButton(request):
-    pass
+    Slvr.solveSudoku(result)
+    jsonStr = json.dumps(result)
+    return JsonResponse(jsonStr, safe=False)
