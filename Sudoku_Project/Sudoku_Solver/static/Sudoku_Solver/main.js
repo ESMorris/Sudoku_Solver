efc737f478a5d6c1548bff85cf2a766621917890
simplydoesnot <emorris.mxi@gmail.com>
2021-10-22 01:21:05 -0700

    Added javascript to dynamically create the tables for the game.

--- Sudoku_Project/Sudoku_Solver/static/Sudoku_Solver/main.js
+++ Sudoku_Project/Sudoku_Solver/static/Sudoku_Solver/main.js
@@ -1,10 +1,32 @@
 // Helper Functions
+//  Should be ran when we generate a new board
 function updateGrid(r, c, state){
-	var sel = "#" + r.toString()+ "-" + c.toString();
-    if (state != 0){
-    }
+	var sel = r.toString()+ "-" + c.toString();
+	if (state != 0){
+		var newInput = document.createElement("input");
+		newInput.type = "text";
+		newInput.id = "s" + r.toString() + "-" + c.toString();
+		newInput.className = "s0"; 
+		newInput.size = "1";
+		newInput.autocomplete = "off";
+		newInput.readOnly = "readonly";
+		newInput.value = state.toString();
+		document.getElementById(sel).appendChild(newInput);
+	}
+	else{
+		var newInput = document.createElement("input");
+		newInput.type = "text";
+		newInput.id = "s" + r.toString() + "-" + c.toString();
+		newInput.className = "s0"; 
+		newInput.size = "1";
+		newInput.autocomplete = "off";
+		newInput.value = "";
+		document.getElementById(sel).appendChild(newInput)
+	}
 }
 
+// Should only be ran once!!!
+// Should create the grid only
 function createGrid(data){
 	var result = "";
 
@@ -14,25 +36,17 @@ function createGrid(data){
 		for (var j in data[i]){
 			cr = cr + "<td id=\"" + i.toString()+ "-" + j.toString() + "\"></td>"
 		}
-
 		cr = cr + "</tr>\n";
 		result = result + cr;
 	}
 	$("#grid").html(result);
 }
 
-
 // Getting the document ready
 $(document).ready(function(){
 
-    // // Changing the background and text to a dark mode theme
-    // // When button is clicked
-    // $("#toggle").click(function(){
-    //     var element = document.body;
-    //     element.classList.toggle("dark-mode");
-    // });
-
-    $.get("Generate/", {}, function(response){
+	// Initialize the starting board when the app is first spun up
+    $.get("Start/", {}, function(response){
 		var data = JSON.parse(response);
 		createGrid(data);
 		for (var row in data){
@@ -40,6 +54,31 @@ $(document).ready(function(){
 				updateGrid(row, col, data[row][col]);
 			}
 		}
+	});
+
+	$("#GenerateBoard").click(function(){
+		$.get("Generate/", {}, function(response){
+			var data = JSON.parse(response);
+			createGrid(data);
+			for (var row in data){
+				for (var col in data[row]){
+					updateGrid(row, col, data[row][col]);
+				}
+			}
+		});
+	});
+
+	$("#SolveBoard").click(function(){
+		$.get("Solve/", {}, function(response){
+			var data = JSON.parse(response);
+			createGrid(data);
+			for (var row in data){
+				for (var col in data[row]){
+					updateGrid(row, col, data[row][col]);
+				}
+			}
+		});
+		window.alert("Solved Sudoku!");
 	});
 
 });
